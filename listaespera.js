dae45102e1a0c4c1318b0468ee2d7a697f3ae5d9
bortmarc8 <bortmarc8@gmail.com>
2019-11-13 17:39:13 +0100

    Añadidas funciones siguiente, borrar, consulta, consulta_pos y guardar

--- listaespera.js
+++ listaespera.js
@@ -1,4 +1,6 @@
-// Función de display del menú
+// Funciones (línea 4 hasta x)
+
+// Función de display del menú.
 function menu() {
     // Variables
     let rl = require('readline-sync');
@@ -6,7 +8,7 @@ function menu() {
     let respuesta = true;
 
     // Display gráfico de la lista de espera.
-    console.log("LISTA ESPERA - Restaurante Florida's Hollywood");    
+    console.log("\nLISTA ESPERA - Restaurante Florida's Hollywood");    
     console.log("===============================================");
     console.log("1. Agregar nuevo cliente a la lista");
     console.log("2. Siguiente cliente ocupa la mesa");
@@ -77,16 +79,161 @@ function agregar(lista) {
     return arreglo;
 }
 
-let lista = [];
+// Función de paso de turno en la cola.
+function siguiente(lista) {
+    let rl = require('readline-sync');
+    let pregunta;
+    let arreglo = lista;
+    let salir;
 
-switch (menu()) {
-    case 1:
-        console.clear();
-        agregar(lista);
-        break;
-
-    default:
-        console.log("Programa finalizado");
+    // Si el array no está vacio imprime la primera posición y eliminala.
+    do {
+        if (arreglo != []) {
+            console.log("Pasa el cliente",arreglo[0],"a la mesa");
+            arreglo = arreglo.shift();
         
-        break;
+        } else {
+            console.log("No hay ninguna lista creada o está vacia, para acceder a esta funcionalidad deberás crear una lista nueva o cargarla.");
+            salir = true;
+        } 
+
+        pregunta = rl.question("Pasar otro = S, salir = Otro: ");
+
+    } while (pregunta.toUpperCase() == 'S');
+    
+    return arreglo;
 }
+
+// Función para borrar clientes impacientes.
+function borrar(lista) {
+    let arreglo = lista;
+    let rl = require('readline-sync');
+    let respuesta;
+    let pregunta;
+
+    do {
+        respuesta = rl.question("Introduce el nombre de la persona a borrar: ");
+        if (arreglo.indexOf(respuesta) != -1) {
+            arreglo = arreglo.splice(arreglo.indexOf(respuesta),arreglo.indexOf(respuesta));
+        
+        } else {
+            console.log("El cliente no está en la cola.");
+                
+        }
+        
+        pregunta = rl.question("Borrar otro = S, salir = Otro: ");
+
+    } while (pregunta.toUpperCase() == 'S');
+    
+    return arreglo;
+
+}
+
+function consulta(lista) {
+    arreglo = lista;
+    if (arreglo != []) {
+        console.log(arreglo)
+    } else {
+        console.log("la lista está vacia");
+        
+    }
+}
+
+// Procedimiento para visualizar el turno de un cliente. (Sin retorno)
+function consulta_pos(lista) {
+    let arreglo = lista;
+    let rl = require('readline-sync');
+    let respuesta;
+    let pregunta;
+
+    do {
+        respuesta = rl.question("Introduce el nombre de la persona para ver su turno: ");
+        if (arreglo.indexOf(respuesta) != -1) {
+            console.log("\n",arreglo[arreglo.indexOf(respuesta)],"está en la posición",arreglo.indexOf(respuesta)+1);
+            
+                
+        } else {
+             console.log("\nEsa persona no está en la lista");
+                
+        }
+    
+        pregunta = rl.question("Consultar otro = S, salir = Otro: ");
+
+    } while (pregunta.toUpperCase() == 'S');
+
+}
+
+// Procedimiento para guardar la lista. (Sin retorno)
+function guardar(lista) {
+    let fs = require('fs');
+    let escritura = fs.createWriteStream('archivo.txt');
+    let ruta = escritura.path;
+    let arreglo = lista;
+    
+    if (lista != []) {
+    
+        // Escribir el dato
+        arreglo.forEach(valor => escritura.write(`${valor}\n`));
+    
+        // Cuando esté terminado mostrar la ruta y verificación
+        escritura.on('finish', () => {
+        console.log(`Se ha guardado la lista en ${ruta}`);
+        });
+    
+        // Mostrar si hay algún error
+        escritura.on('error', (error) => {
+        console.error(`Ha habido un error al guardar la lista en ${ruta} => ${error}`)
+        });
+    
+        // Cerramos la comunicación
+        escritura.end();
+
+    } else {
+        console.log("La lista está vacía, es inecesario guardarla");
+        
+    }
+}
+
+// Comienzo del programa.
+
+let lista = [];
+let salir;
+
+do {
+    switch (menu()) {
+        case 1:
+            console.clear();
+            lista = agregar(lista);
+            break;
+    
+        case 2:
+            console.clear();
+            lista = siguiente(lista);
+            break;
+    
+        case 3: 
+            console.clear();
+            lista = borrar(lista);
+            break;
+
+        case 4:
+            console.clear();
+
+            break;
+
+        case 5:
+            console.clear();
+            consulta_pos(lista);
+            break;
+        
+        case 6:
+            console.clear();
+            guardar(lista);
+            break;
+    
+        default:
+            console.log("Programa finalizado");
+            salir = true;
+            break;
+    }
+} while (!salir);
